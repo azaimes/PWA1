2e63cf4f60ef55db6f69cbacec7aa5b89ba919a6
azaimes <azaimes@fullsail.edu>
2013-09-05 21:33:53 -0400

    created script file and started code

---
+++ Wk1/goal1_duel1/js/main.js
@@ -0,0 +1,26 @@
+/**
+ *
+ * Name: Adam Zaimes
+ * Date: 9/5/13
+ * Assignment: Goal1 Duel1
+ *
+ */
+
+//self-executing function
+(function(){
+    console.log("FIGHT!!!");
+
+    //Player names
+    var playerOneName = "Green Lantern";
+    var playerTwoName = "Hulk";
+
+    //damage
+    var player1Damage = 20;
+    var player2Damage = 20;
+
+    //health
+    var playerOneHealth = 100;
+    var playerTwoHealth = 100;
+
+    //initiate round
+})
