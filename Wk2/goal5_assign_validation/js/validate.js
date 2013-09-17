a697c36596691ed93eae7a794d5c353c6df765c9
azaimes <azaimes@fullsail.edu>
2013-09-17 16:42:16 -0400

    code for validate script about 50% done

--- Wk2/goal5_assign_validation/js/validate.js
+++ Wk2/goal5_assign_validation/js/validate.js
@@ -6,30 +6,36 @@
 */
 
 (function(){
+	var button = document.getElementById('f_submit');    //assigns button to it's ID
 
-    myform.onsubmit = function(e){
+    var myForm = function(e){                           //collects user data
 
-        //Below is one example of the validateField call with an argument.
-        //You must dynamically retrieve the ID name from the DOM/HTML.
+	    //user input from form
+	    var f_username = document.getElementById('f_username');
+	    var f_email = document.getElementById('f_email');
+	    var f_phone = document.getElementById('f_phone');
+	    var f_password = document.getElementById('f_password');
+	    var f_ssn = document.getElementById('f_ssn');
 
-        validateField(id);  //id = is the form input field ID
+	    validateField(f_username); //calls validate function for each user input
+	    validateField(f_email);
+	    validateField(f_phone);
+	    validateField(f_password);
+	    validateField(f_ssn);
 
-
-        e.preventDefault();
-        return false;
+	    e.preventDefault();
+        return false;        //prevents default browser actions
     };
 
 
-    var validateField = function(inputName){
+    var validateField = function(inputData){
 
-        if (inputName.name === 'id name goes here'){
-            var pattern = 'a RegEx pattern goes here'
-
-            //You will need to create an else-if statement for each input field id.  The
-            //      format will be similar to the above IF statement.
-
-
-        };
+        if (inputData.name === 'f_username'){
+	        var regExp = /^[A-Z]+((\s|\-)[a-zA-Z]+)?$/;    //regular expression to force user to use capital letter for first letter of first and last name
+	        }
+	    if (inputData.name === 'f_email'){
+		    var regExp = /^[\w\.=-]+@[\w\.-]+\.[\w]{2,4}$/; // patter from regexlib.com to validate email and check the top level domain is between 2 and 4 characters long
+	    }
 
         var pass = 'the RegEx .test statement is needed here';
         var errorMsg = inputName.nextSibling.nextSibling.nextSibling.nextSibling;
@@ -45,7 +51,10 @@
             inputName.style.backgroundColor = 'white';
         };
     };
+	function onsubmit(){
 
+	}
+	button.onclick=myForm;
 })();  // end wrapper
 
 
