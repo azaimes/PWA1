33e19a3f962055559ba6d97cf8c460ba5125f3f1
azaimes <azaimes@fullsail.edu>
2013-09-23 13:48:57 -0400

    main.js code

---
+++ Wk3/goal7_assign_obj_to_prototypes/js/main.js
@@ -0,0 +1,51 @@
+/**
+ * Name: Adam Zaimes
+ *
+ * Created by the JavaScript Development Team
+ * Class: PWA
+ * Goal: Goal7
+ */
+
+(function(){
+
+	//sets up variables for the arrays and interval
+	var names = ["Monica", "Lacy", "Erick", "Clifton", "Hunter"];
+	var people = [];
+	var interval;
+
+	//for loop
+	for(var i=0; i<3; i++) {
+		var personRandom = Math.floor(Math.random() * names.length);
+		var person = new Person(names[personRandom], i + i);
+
+		//call the function to update cells with names and job
+		populateHTML(person.name, "r" + (i + 1) + "c1");
+		populateHTML(person.job, "r" + (i + 1) + "c2");
+
+		people.push(person);
+		names.splice(personRandom, 1);
+
+		console.log(person);
+	}
+
+	//checks that only one instance runs
+	clearInterval(interval);
+	interval = setInterval(runUpdate, 1000 / 30);
+
+	function runUpdate() {
+		people.forEach(function(element){
+			element.update();
+		})
+	}
+
+	function populateHTML(data, field){
+		var id= document.getElementById(field);
+		id.innerHTML = data;
+	}
+
+
+
+
+
+
+})();
