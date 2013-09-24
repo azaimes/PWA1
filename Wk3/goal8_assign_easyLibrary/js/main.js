48a6904f01b1c990e21730d781b713c3bc06d8a5
azaimes <azaimes@fullsail.edu>
2013-09-23 22:26:42 -0400

    initial commit

---
+++ Wk3/goal8_assign_easyLibrary/js/main.js
@@ -0,0 +1,10 @@
+/*
+	Name: Adam Zaimes
+	Date:9-23-13
+	PWA1: Goal8: Simple Library App
+*/
+
+(function(){
+var anchor = document.querySelectorAll()
+
+})();  // end wrapper
---
+++ Wk3/goal8_assign_easyLibrary/js/ryu.js
@@ -0,0 +1,19 @@
+/*
+	PWA1: Goal8:  Simple Library
+*/
+
+var anchorLib = function(){
+
+    return new anchorLib.prototype.init();
+
+};
+
+anchorLib.prototype = {
+
+    init: function(){},
+
+    elements: []
+
+}; // end prototype
+
+anchorLib.prototype.init.prototype = anchorLib.prototype;
