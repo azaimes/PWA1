1a3d780a776172bcf44217c3e7bf6caf2afcb04e
azaimes <azaimes@fullsail.edu>
2013-09-23 22:32:12 -0400

    main.js

--- Wk3/goal8_assign_easyLibrary/js/main.js
+++ Wk3/goal8_assign_easyLibrary/js/main.js
@@ -5,6 +5,14 @@
 */
 
 (function(){
-var anchor = document.querySelectorAll()
+var anchor = document.querySelectorAll("a"); //gets all of the anchor elements
+
+	console.log('In the Init Function - Output of the elements: '+ anchor); //output of the anchor variable
+
+var results = anchorLib(anchor); //call from ryu
+
+	results.eachElem(function(){
+
+	});
 
 })();  // end wrapper
