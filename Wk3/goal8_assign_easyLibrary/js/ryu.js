f6ca9b626defc1dbf8a5f7bc3aeaa020eddb0248
azaimes <azaimes@fullsail.edu>
2013-09-23 22:36:16 -0400

    ryu.js

--- Wk3/goal8_assign_easyLibrary/js/ryu.js
+++ Wk3/goal8_assign_easyLibrary/js/ryu.js
@@ -2,18 +2,36 @@
 	PWA1: Goal8:  Simple Library
 */
 
-var anchorLib = function(){
+var anchorLib = function (parts) {
 
-    return new anchorLib.prototype.init();
+	return new anchorLib.prototype.init(parts);
 
 };
 
 anchorLib.prototype = {
 
-    init: function(){},
+	init: function (parts) {
+		this.elements = parts;
+	},
 
-    elements: []
+	elements: [],
+
+	eachElem: function (callback) {
+		for (var i = 0, max = this.elements.length; i < max; i++) {
+			callback.call(this.elements[i]);
+			this.elements[i].style.backgroundColor = "grey";
+			console.log('in for loop: '+this.elements[i]);
+
+			this.elements[i].onclick = function (e) {
+				console.log('in for loop onclick: '+this.getAttribute('href'));
+
+				e.preventDefault();
+
+			};
+		}
+	}
 
 }; // end prototype
+
 
 anchorLib.prototype.init.prototype = anchorLib.prototype;
