cd1bba1a1a5495b168658537a2debe5663dbaaf3
azaimes <azaimes@fullsail.edu>
2013-09-24 20:25:08 -0400

    init. commit plus some code

---
+++ Wk3/goal9_assign_canvaschart/js/main.js
@@ -0,0 +1,23 @@
+/**
+ * Created by:
+ * Class: PWA1
+ * Goal: Goal9
+ */
+
+console.log("start canvas");
+
+(function(){
+	var canvas = document.createElement("canvas");
+	document.body.appendChild(canvas);
+	var ctx = canvas.getContext('2d');
+	canvas.width = 400;
+	canvas.height = 300;
+	var chartData = new Array(55, 77, 89, 100);
+
+	var maxBarHeight = 200;
+
+	var img = new Image();
+	img.src =
+
+
+})();
