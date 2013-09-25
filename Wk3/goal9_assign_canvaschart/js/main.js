44d61312a1ef73f2fdf2fc89a782b512ef9b98d3
azaimes <azaimes@fullsail.edu>
2013-09-24 21:06:26 -0400

    code thru step 4

--- Wk3/goal9_assign_canvaschart/js/main.js
+++ Wk3/goal9_assign_canvaschart/js/main.js
@@ -17,7 +17,30 @@ console.log("start canvas");
 	var maxBarHeight = 200;
 
 	var img = new Image();
-	img.src =
+	img.src = "images/bg.jpg";
+	img.addEventListener("load", function () {
+		ctx.drawImage(img, 0,0,400,300);
+		drawBars();
+	}, false);
+
+	var drawBars = function(){
+		ctx.font = "14px Georgia";
+		for(var i = 0, maxdata = chartData.length; i<maxdata; i++){
+		ctx.fillStyle = "rgba(100,200,200,0.75)";
+		var height = maxBarHeight*chartData[i]/100;
+		ctx.rect(i*80+90,270-height,50,height);
+			ctx.fill();
+			ctx.fillStyle = "rgb(255,255,255)";
+			ctx.fillText(chartData[i],i*85+100,50);
+			drawChartText();
+
+
+		}
+	}
+
+
+
+
 
 
 })();
